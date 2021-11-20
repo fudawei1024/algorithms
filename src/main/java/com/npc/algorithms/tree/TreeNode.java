58137408a3cb8b53a9432ba0c3665b539cf78135
fudawei02 <fudawei02@meituan.com>
2021-11-20 16:25:42 +0800

    二叉树

---
+++ src/main/java/com/npc/algorithms/tree/TreeNode.java
@@ -0,0 +1,28 @@
+package com.npc.algorithms.tree;
+
+import jdk.nashorn.internal.objects.annotations.Constructor;
+
+/**
+ * @projectName: algorithms
+ * @package: com.npc.algorithms.tree
+ * @className: TreeNode
+ * @author: fudawei
+ * @description: TODO
+ * @date: 2021/11/20 16:08
+ * @version: 1.0
+ */
+
+public class TreeNode {
+    TreeNode left;
+    TreeNode right;
+    int val;
+
+    public TreeNode(TreeNode left, TreeNode right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
