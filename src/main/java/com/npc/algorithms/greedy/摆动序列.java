d37b4ac3f0161d0beb63f9b11b4c8560b1070780
fudawei02 <fudawei02@meituan.com>
2021-12-29 00:23:27 +0800

    分发饼干、全排列、摇摆序列、递增子序列

---
+++ src/main/java/com/npc/algorithms/greedy/摆动序列.java
@@ -0,0 +1,28 @@
+package com.npc.algorithms.greedy;
+
+
+public class 摆动序列 {
+    public static void main(String[] args) {
+        int c = new 摆动序列().wiggleMaxLength(new int[]{1,17,5,10,13,15,10,5,16,8});
+        System.out.println(c);
+    }
+    public int wiggleMaxLength(int[] nums) {
+        if (nums == null) {
+            return 0;
+        }
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+        int preDiff = 0;
+        int curDiff = 0;
+        int result = 1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            curDiff = nums[i + 1] - nums[i];
+            if ((curDiff > 0 && preDiff <= 0) || (curDiff < 0 && preDiff >= 0)) {
+                result ++;
+                preDiff = curDiff;
+            }
+        }
+        return result;
+    }
+}
