8e804fb57e04c72c6bb0377ffa31fce2de34afd8
erick <fudawei1024@163.com>
2023-03-31 18:42:19 +0800

    堆排序

--- src/main/java/com/npc/algorithms/算法学习/sort/HeapSort.java
+++ src/main/java/com/npc/algorithms/算法学习/sort/HeapSort.java
@@ -1,11 +1,11 @@
-package com.npc.algorithms.海外码农算法.数组.sort;
+package com.npc.algorithms.算法学习.sort;
 
 import com.npc.algorithms.util.Util;
 
 /**
  * @className: QuikSort
  * @author: fudawei
- * @description: TODO
+ * @description: 堆排序
  * @date: 2022/2/28 15:19
  * @version: 1.0
  */
@@ -14,42 +14,48 @@ import com.npc.algorithms.util.Util;
 public class HeapSort {
 
     public static void main(String[] args) {
+
         int[] array = Util.randomNumbers();
         Util.p(array);
         for (int i = 0; i < array.length; i++) {
-            heapInsert(array, i);
+            heapInsæert(array, i);
         }
+        Util.p(array);
 
         int heapSize = array.length;
         swap(array, 0, --heapSize);
+
+        Util.p(array);
         while (heapSize > 0) {
             heapify(array, 0, heapSize);
-            swap(array, 0, -- heapSize);
+            swap(array, 0, --heapSize);
+           // Util.p(array);
         }
-        Util.p(array);
-    }
 
-    private static void heapInsert(int[] arr, int index) {
-        while (arr[index] > arr[(index - 1) / 2]) {
-            swap(arr, index, (index - 1) / 2);
-            index = (index - 1) / 2;
-        }
+        Util.p(array);
     }
 
     private static void heapify(int[] array, int index, int heapSize) {
         int leftChild = index * 2 + 1;
-
         while (leftChild < heapSize) {
-            int largest = (leftChild + 1) < heapSize && array[leftChild] < array[leftChild + 1] ? leftChild + 1  : leftChild;
-            largest = array[largest] > array[index] ? largest : index;
+            int rightChild = leftChild + 1;
+            int largest = rightChild < heapSize && array[leftChild] < array[rightChild] ?  rightChild : leftChild;
+
+            largest = array[index] > array[largest] ? index : largest;
 
             if (largest == index) {
                 break;
             }
-
-            swap(array, largest, index);
+            swap(array, index, largest);
             index = largest;
             leftChild = index * 2 + 1;
+        }
+    }
+
+    private static void heapInsæert(int[] array, int index) {
+        while (array[index] > array[(index - 1) / 2]) {
+            swap(array, index, (index - 1) / 2);
+            index = (index - 1) / 2;
         }
     }
 
